e3d5938992f8d65aa3db53471202ce305e14631e
Glucoza <glucoza666@gmail.com>
2016-08-17 15:23:21 +0200

    Add UI test

---
+++ mobile/src/androidTest/java/com/example/my/circlecitest/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.my.circlecitest;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.level)).check(matches(withText("Level 1")));
+    }
+}
+
